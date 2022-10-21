153cd1f8a10e89897d032c2193cba89823232dcc
Wetoria <>
2022-10-21 16:26:30 +0800

    实现管理系统布局

---
+++ src/components/Layout/ManagementLayout.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div class="left"
+      style="
+        width: 290px;
+        height: 100%;
+        background: white;
+      "
+    ></div>
+    <div class="right"
+      style="
+        width: calc(100% - 290px);
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+      "
+    >
+      <div
+        style="
+          width: 100%;
+          height: 50px;
+          background: white;
+        "
+      ></div>
+      <div
+        style="
+         width: 100%;
+         height: calc(100% - 50px);
+        "
+      >
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.container {
+  width: 100%;
+  height: 100%;
+  background: #EEF0F3;
+  display: flex;
+}
+
+</style>
