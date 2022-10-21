4b80d68461c2be912eff47e4e4760340d04e90b3
Wetoria <>
2022-10-21 16:13:10 +0800

    实现单页布局

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
 <template>
-  <div style="width: 300px; height: 200px; border: 1px solid; overflow: hidden;">
-    <RouterView />
-  </div>
+  <SinglePageLayout />
 </template>
+
+<script>
+import SinglePageLayout from './components/Layout/SinglePageLayout.vue'
+
+export default {
+  components: {
+    SinglePageLayout,
+  }
+}
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+
+html, body, #app {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/components/Layout/SinglePageLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+.container {
+  width: 100%;
+  height: 100%;
+}
+</style>
