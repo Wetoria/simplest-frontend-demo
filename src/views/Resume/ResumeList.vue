a62366ab7be0c01c70f5ff40eb6b9354b98255a0
Wetoria <>
2022-10-22 10:04:00 +0800

    完成简历内容的增删查改及保存

--- src/views/Resume/ResumeList.vue
+++ src/views/Resume/ResumeList.vue
@@ -1,20 +1,65 @@
 <template>
   <h4 v-if="level == 4">{{ title }}</h4>
   <h2 v-else>{{ title }}</h2>
-  <ul>
-    <li v-for="item of list" :key="item">
-      {{ item }}
+  <ul :class="editing ? 'editing' : ''">
+    <li v-for="(item, index) of list" :key="index">
+      <el-row v-if="editing">
+        <el-col :span="22">
+          <el-input :modelValue="item" @input="handleInput(index, $event)" />
+        </el-col>
+        <el-col :span="2">
+          <el-row align="middle" style="height: 100%;">
+            <el-button
+              class="opt-btn"
+              style="margin-left: 8px"
+              type="primary"
+              icon="Plus"
+              circle
+              @click="handleAdd(index)"
+            ></el-button>
+            <el-button
+              class="opt-btn"
+              style="margin-left: 8px"
+              type="danger"
+              icon="Minus"
+              circle
+              @click="handleDelete(index)"
+            ></el-button>
+          </el-row>
+        </el-col>
+      </el-row>
+      <span v-else>{{ item }}</span>
     </li>
   </ul>
 </template>
 
 <script>
+
 export default {
-  props: ['list', 'title', 'level']
+  props: ['list', 'title', 'level', 'editing'],
+  data() {
+    return {
+      innerList: this.list,
+    }
+  },
+  methods: {
+    handleInput(index, event) {
+      this.innerList[index] = event
+    },
+    handleAdd(index) {
+      this.innerList.splice(index + 1, 0, '')
+    },
+    handleDelete(index) {
+      this.innerList.splice(index, 1)
+      if (this.innerList.length === 0) {
+        this.innerList.push('')
+      }
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
 h1,
 h2,
 h3,
@@ -36,5 +81,13 @@ h4 {
 ul {
   margin: 16px 0;
   padding-left: 40px;
+}
+ul.editing li {
+  margin-bottom: 8px;
+}
+
+.opt-btn {
+  width: 20px;
+  height: 20px;
 }
 </style>
