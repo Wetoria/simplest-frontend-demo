e841b23c0cf5c551592d57506a8fb840cdcf9047
Wetoria <>
2022-10-22 15:26:59 +0800

    优化 ResumeList 空数组的展示效果

--- src/views/Resume/ResumeList.vue
+++ src/views/Resume/ResumeList.vue
@@ -1,36 +1,38 @@
 <template>
-  <h4 v-if="level == 4">{{ title }}</h4>
-  <h2 v-else>{{ title }}</h2>
-  <ul :class="editing ? 'editing' : ''">
-    <li v-for="(item, index) of finalList" :key="index">
-      <el-row v-if="editing">
-        <el-col :span="22">
-          <el-input :modelValue="item" @input="handleInput(index, $event)" />
-        </el-col>
-        <el-col :span="2">
-          <el-row align="middle" style="height: 100%;">
-            <el-button
-              class="opt-btn"
-              style="margin-left: 8px"
-              type="primary"
-              icon="Plus"
-              circle
-              @click="handleAdd(index)"
-            ></el-button>
-            <el-button
-              class="opt-btn"
-              style="margin-left: 8px"
-              type="danger"
-              icon="Minus"
-              circle
-              @click="handleDelete(index)"
-            ></el-button>
-          </el-row>
-        </el-col>
-      </el-row>
-      <span v-else>{{ item }}</span>
-    </li>
-  </ul>
+  <div v-if="finalList.length">
+    <h4 v-if="level == 4">{{ title }}</h4>
+    <h2 v-else>{{ title }}</h2>
+    <ul :class="editing ? 'editing' : ''">
+      <li v-for="(item, index) of finalList" :key="index">
+        <el-row v-if="editing">
+          <el-col :span="22">
+            <el-input :modelValue="item" @input="handleInput(index, $event)" />
+          </el-col>
+          <el-col :span="2">
+            <el-row align="middle" style="height: 100%;">
+              <el-button
+                class="opt-btn"
+                style="margin-left: 8px"
+                type="primary"
+                icon="Plus"
+                circle
+                @click="handleAdd(index)"
+              ></el-button>
+              <el-button
+                class="opt-btn"
+                style="margin-left: 8px"
+                type="danger"
+                icon="Minus"
+                circle
+                @click="handleDelete(index)"
+              ></el-button>
+            </el-row>
+          </el-col>
+        </el-row>
+        <span v-else>{{ item }}</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
